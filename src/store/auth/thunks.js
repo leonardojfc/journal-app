1b88e6ec65008f032722f68448c6048d7c1fce70
ljferreyrac <lferreyrac04@hotmail.com>
2024-02-04 17:38:30 -0500

    FIX: Cambiando desestructuración en thunks

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -35,7 +35,7 @@ export const startLoginWithEmailPassword = ({email, password}) => {
     return async( dispatch ) => {
         dispatch( checkingCredentials() );
         const result = await loginWithEmailPassword({email, password});
-        if(!result.ok) return dispatch( logout({errorMessage}) );
+        if(!result.ok) return dispatch( logout(result.errorMessage) );
         dispatch( login(result) )
     }
 }
