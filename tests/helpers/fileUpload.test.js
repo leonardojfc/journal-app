0ff86ff3013c94b49f41be31b38ea2ee352ac308
ljferreyrac <lferreyrac04@hotmail.com>
2024-02-04 13:32:04 -0500

    ADD: Test en fileUpload (No cloudinary config)

--- tests/helpers/fileUpload.test.js
+++ tests/helpers/fileUpload.test.js
@@ -1,7 +1,6 @@
 import { v2 as cloudinary } from 'cloudinary'
 import { fileUpload } from "../../src/helpers/fileUpload"
 
-
 describe('Pruebas en el fileUpload', () => { 
     test('should upload the file to cloudinary correctly', async () => { 
         const imageUrl = "https://media.istockphoto.com/id/583809524/es/foto/desierto-de-alberta-cerca-de-banff.jpg?s=612x612&w=0&k=20&c=mvDownbgLZRz1Wci6yV68NRRngqWNrl_lRF6QtrdJkw="
@@ -12,6 +11,15 @@ describe('Pruebas en el fileUpload', () => {
         const url = await fileUpload(file);
 
         expect( typeof url ).toBe('string');
+
+        const segments = url.split('/');
+        const imageId = segments[segments.length - 1].replace('.jpg', '');
+        
+        await cloudinary.api.delete_resources(['journal-app/' + imageId], {
+            resource_type: 'image'
+        });
+
+        
     })    
     test('should return null if not file', async () => { 
 
